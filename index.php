eac2acd88db743129c3346d70e53a54a3d630d81
baharia <bahariap5@gmail.com>
2026-06-25 10:45:47 +0300

    Added login feature to student registration system

--- index.php
+++ index.php
@@ -13,3 +13,24 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     echo "<h3>Welcome $name</h3>";
 }
 ?>
+
+<h2>Login Section</h2>
+
+<form method="POST">
+    Username: <input type="text" name="username"><br><br>
+    Password: <input type="password" name="password"><br><br>
+    <input type="submit" value="Login">
+</form>
+
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    if(isset($_POST['username']) && isset($_POST['password'])){
+        $user = $_POST['username'];
+        $pass = $_POST['password'];
+
+        echo "<h3>Login Successful for: $user</h3>";
+    }
+}
+?>
+
